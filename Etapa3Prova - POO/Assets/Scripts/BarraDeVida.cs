b3876deec56037086ea3c55173fba55cf8d1cc4f
lvinia <145567312+lvinia@users.noreply.github.com>
2025-10-13 16:18:26 -0300

    D13M10

---
+++ Etapa3Prova - POO/Assets/Scripts/BarraDeVida.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraDeVida : MonoBehaviour
+{
+    public Slider sliderVidasRestantes;
+    
+    public Personagem personagem;
+    [SerializeField]
+    private int vidasRestantes = 0;
+    
+  
+    void Start()
+    {
+        if (personagem != null)
+        {
+            if (sliderVidasRestantes != null)
+            {
+                sliderVidasRestantes.minValue = 0;
+                sliderVidasRestantes.maxValue = personagem.getVida();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (sliderVidasRestantes != null)
+        {
+            vidasRestantes = personagem.getVida();
+            sliderVidasRestantes.value = vidasRestantes;
+        }
+    }
+}
