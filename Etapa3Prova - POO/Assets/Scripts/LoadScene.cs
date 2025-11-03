6e90d0b78273542a40eec8763943cddab44c1129
lvinia <145567312+lvinia@users.noreply.github.com>
2025-11-03 10:06:29 -0300

    .

---
+++ Etapa3Prova - POO/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public string sceneName;
+    
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
